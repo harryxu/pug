471ed6e7137aab638fc34893a65c47040bb3f7a0
harryxu <harryzhxu@gmail.com>
2016-12-12 11:54:02 +0800

    fetch api

--- pug/src/actions/index.js
+++ pug/src/actions/index.js
@@ -1,3 +1,10 @@
-import 'whatwg-fetch'
+import {fetch as fetchApi} from 'whatwg-fetch'
 
 
+function fetch(input, init) {
+    init = init || {};
+    init = Object.assign(init, {
+        credentials: 'same-origin'
+    })
+    return fetchApi(input, init);
+}
