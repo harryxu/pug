1f50910ec90d583b10aba7e09856108ebb0880b6
harryxu <harryzhxu@gmail.com>
2016-12-23 14:57:55 +0800

    working on ResponsePane

--- pug/src/containers/ResponsePane.js
+++ pug/src/containers/ResponsePane.js
@@ -1,15 +1,36 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 
-import { Menu, Input, Button } from 'semantic-ui-react'
+import { Menu, Form, Input, Button } from 'semantic-ui-react'
+import Select from 'react-select'
 
 class ResponsePane extends Component {
 
     static contentTypes = [
-
-    ]
+        'application/json',
+        'application/xml',
+        'text/html'
+    ] .map(v => ({value: v, label: v}));
 
     static statusCodes = [
+        // 2xx
+        {value: 200, label: '200 OK'},
+        {value: 201, label: '201 Created'},
+
+        // 3xx
+        {value: 300, label: '300 Multiple Choices'},
+        {value: 301, label: '301 Moved Permanently'},
+        {value: 302, label: '302 Found'},
+
+        // 4xx
+        {value: 400, label: '400 Bad Request'},
+        {value: 401, label: '401 Unauthorized'},
+        {value: 402, label: '402 Payment Required'},
+        {value: 403, label: '403 Forbidden'},
+        {value: 404, label: '404 Not Found'},
+
+        // 5xx
+        {value: 500, label: '500 Internal Server Error'},
 
     ]
 
@@ -22,9 +43,25 @@ class ResponsePane extends Component {
 
     renderResponse() {
         return (
-            <div className="response-detail">
-                <Input label="Name:" />
-            </div>
+            <Form className="response-detail">
+                <Form.Group widths='equal'>
+                    <Form.Input label="Name:" />
+
+                    <div className="field">
+                        <label>Content Type</label>
+                        <Select name="content_type"
+                                options={this.constructor.contentTypes}
+                        />
+                    </div>
+
+                    <div className="field">
+                        <label>Status Code</label>
+                        <Select name="status_code"
+                                options={this.constructor.statusCodes}
+                        />
+                    </div>
+                </Form.Group>
+            </Form>
         )
     }
 
